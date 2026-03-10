a8fa5bc9f07572870042d2721bbec21ad799c0f8
Amyzi <ebiojoidris@icloud.com>
2026-03-10 11:30:14 +0100

    update

--- src/app/api/services/route.ts
+++ src/app/api/services/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getAllServices } from "@/lib/wordpress";
 
-export const revalidate = 60;
+export const revalidate = 0;
 
 export async function GET() {
   try {
