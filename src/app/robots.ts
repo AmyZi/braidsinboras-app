3d5ba96534164f860f3ef5216950a110e5f7ffbd
Amyzi <ebiojoidris@icloud.com>
2026-03-18 10:45:58 +0100

    fix: url error

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin/", "/api/"],
+      disallow: ["/admin/", "/api/admin/", "/api/auth/", "/api/stripe/"],
     },
     sitemap: "https://braidsboras.se/sitemap.xml",
   };
